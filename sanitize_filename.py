dd6e0411943011fd4bdaca09a63337089c8d0504
Shenghang Tsai <jackalcooper@gmail.com>
2020-09-11 21:57:00 +0800

    quick fix

--- sanitize_filename.py
+++ sanitize_filename.py
@@ -1,7 +1,10 @@
 from pathvalidate import sanitize_filename
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="Process some integers.")
-parser.add_argument("--img_name", required=True, help="docker img name")
+parser.add_argument(
+    "--img_name", required=True, help="docker img name", default=os.getenv("IMG_NAME")
+)
 args = parser.parse_args()
 print(sanitize_filename(args.img_name))
