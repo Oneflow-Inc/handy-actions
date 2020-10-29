b2c85ec133e4adf8349aca32ad12debd9bc500fa
Shenghang Tsai <jackalcooper@gmail.com>
2020-10-29 20:48:07 +0800

    add script to load

--- load_img.py
+++ load_img.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     oss_url = args.oss_url
     basename = os.path.basename(args.oss_url)
-    dl_cmd = f"""{ossutil_bin} cp -r {oss_url} {basename}
+    dl_cmd = f"""set -x
+{ossutil_bin} cp -r {oss_url} {basename}
 {ossutil_bin} cp {oss_url}.tag {basename}.tag
 docker run --rm -v $PWD:$PWD -w $PWD ananace/skopeo copy dir:./{basename}  docker-archive:./{basename}.tar
 """
@@ -21,5 +22,9 @@ docker run --rm -v $PWD:$PWD -w $PWD ananace/skopeo copy dir:./{basename}  docke
     # Loaded image ID: sha256:f0b02e9d092d905d0d87a8455a1ae3e9bb47b4aa3dc125125ca5cd10d6441c9f
     loaded_output = subprocess.check_output("docker load -i {basename}.tar", shell=True)
     sha256 = loaded_output.split(":")[-1]
-    tag_cmd = f"docker tag {sha256} $(cat {basename}.tag)"
+    tag = None
+    with open("{basename}.tag") as f:
+        tag = f.read()
+    assert tag
+    tag_cmd = f"docker tag {sha256} {tag}"
     subprocess.check_call(tag_cmd, shell=True)
