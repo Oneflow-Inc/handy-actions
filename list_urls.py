2be6d67627aee6b6e553111c9f3361803414c75e
tsai <jackalcooper@gmail.com>
2023-11-15 19:06:50 +0800

    fix

--- list_urls.py
+++ list_urls.py
@@ -14,7 +14,7 @@ aliyun_base_url = "oss://oneflow-static/hf_hub/LCM_Dreamshaper_v7"
 
 repo_path = "/Users/tsai/Downloads/lcm-lora-sdxl"
 repo = Repo(repo_path)
-dl_base_url = "https://huggingface.co/latent-consistency/lcm-lora-sdxl"
+dl_base_url = "https://huggingface.co/latent-consistency/lcm-lora-sdxl/resolve/main/"
 aliyun_base_url = "oss://oneflow-static/hf_hub/lcm-lora-sdxl"
 
 # Get the list of all tracked files
